dd09c934463ddf94a1b8829583c4c25d66936dba
gouldbenney <gould.benney@outlook.com>
2020-12-01 06:32:42 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react'
-import './App.css';
 import Router from './router'
 import {BrowserRouter} from 'react-router-dom'
 import './Styles/Log.css'
-function App() {
+import Login from './Pages/LoginPage'
+import SignUp from './Pages/SignUpPage'
+import Weather from './Pages/Weather'
+
+function Weather() {
   return (
+    <div>
    <BrowserRouter>
       <Router/>
    </BrowserRouter>
+   </ Log>
+</div>
   );
 }
 
-export default App;
+export default Weather;
--- src/Pages/index.js
+++ src/Pages/index.js
@@ -4,6 +4,15 @@ const Login=React.lazy(()=> import('./LoginPage'))
 const Weather =React.lazy(()=> import('./Weather'))
 const SignUp =React.lazy(()=> import('./SignUpPage'))
 
+ReactDOM.render(
+    <React.StrictMode>
+        <BrowserRouter>
+        <App />
+        </BrowserRouter>
+    </React.StrictMode>
+    document.getElementById('root')
+)
+
 const Pages ={
     Login,
     Weather,
